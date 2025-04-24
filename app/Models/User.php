48be085ca952d83a9971ca45ba0c7841f6f22648
mariam <mariamemampp00@gmail.com>
2025-04-24 16:16:24 +0200

    make relation between user and tweet table

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class);
+    }
 }
