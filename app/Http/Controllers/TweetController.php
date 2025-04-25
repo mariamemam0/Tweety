87c27a97ed8b20edcb8eee84cf42a6da1d43e27b
mariam <mariamemampp00@gmail.com>
2025-04-25 16:57:57 +0300

    make crud for tweets

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 
 class TweetController extends Controller
 {
-    //
+    public function index()
+    {
+        $tweets = Tweet::with('user')->latest()->get();
+        return view('tweete.index', compact('tweets'));
+
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'body' => 'required|max:255',
+        ]);
+
+        auth()->user()->tweets()->create(
+            [
+                'body' => $request->body,
+            
+            ]
+        );
+        return redirect()->back();
+
+        
+    }
 }
