1443dfa1616fde034f6a28d795a9da3047374f97
mariam <mariamemampp00@gmail.com>
2025-04-26 21:21:20 +0300

    add show function to show user's profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -57,4 +58,10 @@ class ProfileController extends Controller
 
         return Redirect::to('/');
     }
+     public function show(User $user)
+     {
+        return view('profiles.show',[
+            'user'=>$user      
+        ]);
+     }
 }
