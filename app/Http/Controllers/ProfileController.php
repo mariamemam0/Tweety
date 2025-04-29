3b333351f062916184203c9ecd2287acf0fe72be
mariam <mariamemampp00@gmail.com>
2025-04-29 14:49:04 +0300

    show followers and following in the profile of the user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -60,9 +60,10 @@ class ProfileController extends Controller
     }
      public function show(User $user)
      {
-        return view('profiles.show',[
-            'user'=>$user      
-        ]);
+
+        $followers = $user->followers;
+        $following = $user->following;
+        return view('profiles.show',compact('user','followers','following'));
 
      }
       
