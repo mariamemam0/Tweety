cc03e9ba4e0687ab855381b563a46e1469dcd3b0
mariam <mariamemampp00@gmail.com>
2025-04-29 14:49:31 +0300

    add time to the data of the relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,11 +58,11 @@ class User extends Authenticatable
 
     public function followers()
     {
-        return $this->belongsToMany(User::class, 'follows','user_id', 'following_user_id');
+        return $this->belongsToMany(User::class, 'follows','user_id', 'following_user_id')->withTimestamps();
 
     }
     public function following()
     {
-        return $this->belongsToMany(User::class, 'follows','following_user_id', 'user_id');
+        return $this->belongsToMany(User::class, 'follows','following_user_id', 'user_id')->withTimestamps();
     }
 }
